9e7be7ec72f3d251800b42ee1ab997126b4b8230
tvoigoroshek <92248290+tvoigoroshek@users.noreply.github.com>
2021-10-10 11:46:36 +0300

    new hw

---
+++ ру 4.py
@@ -0,0 +1,14 @@
+s1=''
+while s1.isdigit() is False:
+    s1 = input("Введите число 1:")
+    s2=""
+while s2.isdigit() is False:
+     s2 =input("Введите число 2:")
+     s3=int(s1)+int(s2)
+     print(s3)
+
+s1=input("Введите данные: ")
+if s1 == s1:
+    print("Ok")
+if s1== "" :
+ print(" ")
